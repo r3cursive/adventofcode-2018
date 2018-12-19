3a4553b9600e400239dcfb913e9a4ae394759c9c
Kirk Pinto <f1dude@gmail.com>
2018-12-18 20:56:12 -0800

    pt1 is good, rebuilding for pt2

--- day2/main.go
+++ day2/main.go
@@ -1,1 +1,49 @@
 package main
+
+import (
+	"bufio"
+	"fmt"
+	"github.com/sergi/go-diff/diffmatchpatch"
+	"os"
+	"strings"
+)
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func main() {
+	fmt.Println("hi")
+	dat, err := os.Open("./input")
+	check(err)
+	defer dat.Close()
+
+	//letters = map[string]int{}
+	three_times := 0
+	two_times := 0
+	scanner := bufio.NewScanner(dat)
+	alpha := "abcdefghijklmnopqrstuvwxyz"
+	for scanner.Scan() {
+		text := scanner.Text()
+		check(err)
+		for _, elem := range alpha {
+			if strings.Count(text, string(elem)) == 3 {
+				three_times++
+				break
+			}
+		}
+
+		for _, elem := range alpha {
+			if strings.Count(text, string(elem)) == 2 {
+				two_times++
+				break
+			}
+
+		}
+	}
+
+	fmt.Println(three_times*two_times, " Result")
+
+}
